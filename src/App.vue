fe4898dda877dbc55bf415baecb001de3f3c1363
Vinny <damo_m1_max@M1-Max-MBP.attlocal.net>
2022-01-09 10:50:19 -0800

    Clean up Leaderboard UI and add % dead

--- src/App.vue
+++ src/App.vue
@@ -38,12 +38,15 @@
 
         <div class="container-fluid">
                 <div class="row mt-4">
-                    <div class="col-7 offset-1">
+                    <div class="col-8">
                     <router-view/>
                     </div>
                     <div class="col-4 small_screen_hidden">
-                        <img src="@/assets/arena.gif" class="img">
-                        by <a href="https://twitter.com/tyandrykowski" target="_blank">@tyandrykowski</a>
+                        <div class="float_right">
+                            <img src="@/assets/arena.gif" class="img" width="400px;">
+                            <br>
+                            by <a href="https://twitter.com/tyandrykowski" target="_blank">@tyandrykowski</a>
+                        </div>
                     </div>
                 </div>
         </div>
