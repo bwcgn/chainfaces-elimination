696a06d65a2d78c873a7c44536222ffae7035fc1
Glenn Eggleton <geggleto@gmail.com>
2022-04-05 23:44:39 -0400

    up

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,15 +4,6 @@
             <router-link class="nav-link" to="/">HOME</router-link>
         </li>
         <li class="nav-item">
-            <router-link class="nav-link" to="/leaders">LEADERS</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/eliminated">ELIMATED</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/cowards">COWARDS</router-link>
-        </li>
-        <li class="nav-item">
             <router-link class="nav-link" to="/timemachine">TIME MACHINE</router-link>
         </li>
         <li class="nav-item">
